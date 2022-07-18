e3324eaa0d6e5846a39e8e93d42311a6f984a6a3
lmells98 <leightn.mells23@gmail.com>
2022-07-18 17:54:54 +0930

    Class Gen open HPP

--- includes/GenClass.hpp
+++ includes/GenClass.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <cstring>
 
@@ -6,7 +7,38 @@ class GenClass
 {
 	public:
 		GenClass( void ) {}
-		GenClass( int c, char **v );
+		GenClass( int c, char **v ) {
+			string	option;
+
+			if (c < 2 || c > 3)
+			{
+				cerr<<"Usage: ./GenClass \"Class Name\" (y/yes) - optional arg for virtual class"<<endl;
+				return ;
+			}
+			ClassName = CopyValidString(v[0]);
+			if (ClassName.empty())
+			{
+				cerr<<"Class Name Error: `"<<*v<<"': must start with an alhabetical character or un underscore"<<endl;
+				return ;
+			}
+			IsVirtual = false;
+			if (c == 3)
+			{
+				option = CopyValidString(v[1]);
+				if (option.compare("yes") == 0 && option.compare("y") == 0)
+				{
+					cout<<"Generating " + ClassName + " with virtual option on"<<endl;
+					IsVirtual = true;
+				}
+				else
+				{
+					cerr<<"Warning: " + option + ": Unrecognised setting (y/yes)"<<endl;
+					cerr<<"Warning: Virtual class will not be set"<<endl;
+				}
+			}
+			else
+				cout<<"Generating " + ClassName + " with virtual option off"<<endl;
+		}
 		~GenClass( void ) {}
 
 		std::string	GetClassName( void ) {return (ClassName);}
--- srcs/GenClass.cpp
+++ srcs/GenClass.cpp
@@ -22,37 +22,13 @@ string	CopyValidString(const char *__c) {
 	return (__s);
 }
 
-GenClass::GenClass(int c, char **v) {
-	string	option;
+static void	CreateHPP(GenClass &gen)
+{
+	fstream	ClassHPP;
 
-	if (c < 2 || c > 3)
-	{
-		cerr<<"Usage: ./GenClass \"Class Name\" (y/yes) - optional arg for virtual class"<<endl;
-		return ;
-	}
-	ClassName = CopyValidString(v[0]);
-	if (ClassName.empty())
-	{
-		cerr<<"Class Name Error: `"<<*v<<"': must start with an alhabetical character or un underscore"<<endl;
-		return ;
-	}
-	IsVirtual = false;
-	if (c == 3)
-	{
-		option = CopyValidString(v[1]);
-		if (option.compare("yes") == 0 && option.compare("y") == 0)
-		{
-			cout<<"Generating " + ClassName + " with virtual option on"<<endl;
-			IsVirtual = true;
-		}
-		else
-		{
-			cerr<<"Warning: " + option + ": Unrecognised setting (y/yes)"<<endl;
-			cerr<<"Warning: Virtual class will not be set"<<endl;
-		}
-	}
-	else
-		cout<<"Generating " + ClassName + " with virtual option off"<<endl;
+	ClassHPP.open(gen.GetClassName() + ".hpp");
+	
+	ClassHPP.close();
 }
 
 int	main(int c, char **v) {
@@ -64,7 +40,7 @@ int	main(int c, char **v) {
 		return (1);
 	}
 	cout<<"New class name = " + gen->GetClassName()<<endl;
+	CreateHPP(*gen);
 	delete gen;
 	return (0);
-
 }
